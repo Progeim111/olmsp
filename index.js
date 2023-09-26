d8166fabe8c95d3aa48d8db04db7b4e835f15e64
raitsOOT <Rait.soot@voco.ee>
2023-09-26 10:32:48 +0300

    Error in registration

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
+const crypto = require('crypto');
+
 require('dotenv').config();
 
 const connection = mysql.createConnection({
@@ -45,41 +47,34 @@ app.post('/register', async (req, res) => {
 
     console.log(username, email, password)
     // Check if there are matching usernames or emails in the database
-    const username_query = `SELECT *
-                            FROM olmsp.users_info
-                            WHERE name = '${username}'`;
-    const email_query = `SELECT *
-                         FROM olmsp.users_info
-                         WHERE email = '${email}'`;
+    const username_query = 'SELECT name FROM olmsp.users_info WHERE name = ?';
+    const email_query = 'SELECT email FROM olmsp.users_info WHERE email = ?';
+
+
 
     try {
 
-        const username_result = await query(username_query);
-        const email_result = await query(email_query);
-        const emailInfo = await verifyEmail(email);
 
-        console.log(username_result, email_result)
+        const email_result = await connection.query(email_query, [email]);
+        const username_result = await connection.query(username_query, [username]);
+
         if (username_result.length > 0) {
-            res.status(400).send('Username already exists');
+            res.send('Username already exists');
+            console.log(username_result)
         } else if (email_result.length > 0) {
-            res.status(400).send('Email already exists');
-        } else
-            // Verify the email
-        if (emailInfo.smtpCheck !== 'true') {
-            res.status(400).send('Invalid email address');
-        } else {
+            res.send('Email already exists');
+            console.log(email_result)
+        }  else {
 
 
             // Hash the password
             const hashedPassword = await bcrypt.hash(password, 10);
 
             // Insert the new user into the database
-            const insert_query = `INSERT INTO olmsp.users_info (name, email, password)
-                                  VALUES ('${username}', '${email}', '${hashedPassword}')`;
-            const insert_result = await query(insert_query);
-            console.log(insert_result)
+            const insert_query = 'INSERT INTO olmsp.users_info (name, email, password) VALUES (?, ?, ?)';
+            const insert_result = await connection.query(insert_query, [username, email, hashedPassword]);
             if (insert_result.affectedRows === 1) {
-                res.status(201).send('User registered');
+                res.status(202).send('User registered');
             } else {
                 res.status(400).send('Could not register user');
             }
@@ -92,6 +87,38 @@ app.post('/register', async (req, res) => {
 })
 
 
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+
+    const username_query = 'SELECT name FROM olmsp.users_info WHERE name = ?';
+    const password_query = 'SELECT password FROM olmsp.users_info WHERE name = ?';
+
+    try {
+        const username_result = await connection.query(username_query, [username]);
+        const password_result = await connection.query(password_query, [username]);
+
+
+        if (username_result.length === 0) {
+            res.status(400).send('Username or password does not exist');
+        } else {
+            const hashedPassword = password_result[0].password;
+            const passwordCorrect = await bcrypt.compare(password, hashedPassword);
+            if (passwordCorrect) {
+                crypto.randomBytes(64, (err, buf) => {
+                    if (err) throw err;
+                    const token = buf.toString('hex');
+                    res.status(201).send(token);
+                });
+            } else {
+                res.status(400).send('Password does not exist');
+            }
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal server error');
+    }
+
+})
 // Serve the HTML file
         app.get('/', (req, res) => {
             res.sendFile(path.join(__dirname, 'index.html'));
@@ -101,17 +128,3 @@ app.post('/register', async (req, res) => {
         app.listen(port, () => {
             console.log(`App running. Docs at http://localhost:${port}/`);
         });
-
-
-        function verifyEmail(email) {
-            return new Promise((resolve, reject) => {
-                verifier.verify(email, (err, info) => {
-                    console.log(err, info);
-                    if (err) {
-                        reject(JSON.stringify(err));
-                    } else {
-                        resolve(info);
-                    }
-                });
-            });
-        }
